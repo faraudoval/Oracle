27fee6b49ab23b4d743e3043ad3d95e7e654b95d
faraudoval <99222462+faraudoval@users.noreply.github.com>
2024-01-17 16:22:40 -0300

    Add files via upload
    Desafío: hora de practicar

---
+++ 1 java/app.js
@@ -0,0 +1,15 @@
+
+alert('¡Bienvenida y bievenido a nuestro sitio web!');
+let nombre = "Luna";
+let edad = 25;
+let numeroDeVentas = 50;
+let saldoDisponible = 1000;
+alert('¡Error! Completa todos los campos');
+let mensajeDeError = "¡Error! Completa todos los campos";
+alert(mensajeDeError);
+let nombre1 = prompt('Ingrese el nombre de usuario');
+let edad1 = prompt('Ingrese la edad del usuario');
+
+if (edad1 >= 18) {
+    alert('¡Puedes obtener tu licencia de conducir!');
+} 
