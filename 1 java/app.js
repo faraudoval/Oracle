d90ddc67d2085e45979ff09a19e8b2b82f2ed0ef
faraudoval <99222462+faraudoval@users.noreply.github.com>
2024-01-18 14:23:24 -0300

    Update app.js

--- 1 java/app.js
+++ 1 java/app.js
@@ -13,3 +13,27 @@ let edad1 = prompt('Ingrese la edad del usuario');
 if (edad1 >= 18) {
     alert('¡Puedes obtener tu licencia de conducir!');
 } 
+
+let dia = prompt('¿Que dia de la semana es?');
+if (dia=('Sabado'|| 'sabado'||'Domingo' ||'domingo')){
+    alert('¡Buen fin de semana!');
+} else {
+    alert('¡Buena semana!');
+}
+let num = prompt('Ingresa un numero');
+if (num<0){
+    alert('El numero es negativo!');
+} else { 
+    alert('El numero es positivo!');
+}
+let punt = prompt('Ingrese la puntuacion ');
+if (punt>=100){
+    alert('¡Felicidades, has ganado!');
+} else {
+    alert('Intenta nuevamente para ganar.');
+}
+let nombre = prompt('Ingrese su nombre!');
+alert('Bienvenido'+ nombre);
+
+let saldoCuenta = 500; 
+alert(`Tu saldo es de $${saldoCuenta}.`); //templatestring
